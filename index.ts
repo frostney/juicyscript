c2b94c5da6440e10f0539bfd0d76e52965257210
Johannes Stein <johannesstein@freeze-dev.com>
2022-12-12 19:44:48 +0000

    Performance measurements

--- index.ts
+++ index.ts
@@ -2,19 +2,29 @@ import { Lexer } from "./Lexer";
 import { Parser } from "./Parser";
 import { Interpreter } from "./Interpreter";
 
+const t0 = performance.now();
 const input = `add = (a, b) => a + b`;
 const lexer = new Lexer(input);
 const tokens = lexer.lex();
+const t1 = performance.now();
 
 console.log(tokens);
 
 const parser = new Parser(tokens);
 const ast = parser.parse();
+const t2 = performance.now();
 
 console.log(ast);
 
 const interpreter = new Interpreter(ast);
 console.log(interpreter);
+
+const t3 = performance.now();
+
+console.log(`Lexer: ${(t1 - t0).toFixed(3)}ms`);
+console.log(`Parser: ${(t2 - t1).toFixed(3)}ms`);
+console.log(`Interpreter: ${(t3 - t2).toFixed(3)}ms`);
+console.log(`Total: ${(t3 - t0).toFixed(3)}ms`);
 
 // const code = `
 //   add = (a, b) => a + b
